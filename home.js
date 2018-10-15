9e5945d875f7347aada7383c0b8d5ad263a94cd9
alanoudssr <alanoudssr@gmail.com>
2018-10-15 10:01:31 +0300

    created a landing start page

---
+++ home.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+    // Run code
+    $('path').css('stroke-dashoffset', '0');
+
+
+    $('#start').on('click', function () {
+        window.location.href = "index.html";
+    });
+
+}); 
