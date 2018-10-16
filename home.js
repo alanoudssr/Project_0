5518e7383dfc58d8a19cb48cccd891c856a4ee29
alanoudssr <alanoudssr@gmail.com>
2018-10-16 12:22:15 +0300

    added some animate css to index

--- home.js
+++ home.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   // Run code
   $("path").css("stroke-dashoffset", "0");
+  window.setTimeout(() => {
+    $("#startOne").css("display", "inline-block");
+    $("#startTwo").css("display", "inline-block");
+    $("#startOne").addClass("slideInLeft");
+    $("#startTwo").addClass("slideInRight");
+  }, 2600);
 
   $("#startOne").on("click", function() {
     window.location.href = "onePlayer.html";
