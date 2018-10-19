3fa4e6aee3d99c681dce8ace9702cb5381cc608a
alanoudssr <alanoudssr@gmail.com>
2018-10-19 19:23:53 +0300

    made js DRY and added more comments

--- onePlayer.js
+++ onePlayer.js
@@ -1,25 +1,50 @@
 $(document).ready(function () {
 
+  // check if the player had previously written their name before --> Improving UX
   if (sessionStorage.getItem("playerOne") !== null) {
     $("#playerOne").val(sessionStorage.getItem("playerOne"));
   }
 
-  //landing popup! Promotes user to choose a grid size
+  // score setting
+  var scoreXCount = $("#numberOfXWins");
+  var scoreOCount = $("#numberOfOWins");
+  if (sessionStorage.getItem("xAIScore") === null) {
+    sessionStorage.setItem("xAIScore", 0);
+  }
+  var xCurrentScore = parseInt(sessionStorage.getItem("xAIScore"));
+  if (sessionStorage.getItem("oAIScore") === null) {
+    sessionStorage.setItem("oAIScore", 0);
+  }
+  var oCurrentScore = parseInt(sessionStorage.getItem("oAIScore"));
+
+
+  //initiate landing popup! Shows up on load
   var gridPopUp = document.getElementById("gridPopup");
   var span = document.getElementsByClassName("close")[0];
   gridPopUp.style.display = "block";
 
+  //initiate popup window that displays the result
+  var popUp = document.getElementById("winPopup");
+  var span = document.getElementsByClassName("close")[0];
+
   //variables needed
   var arr3 = [];
-  var moves;
+  var moves = 0;
   var random = 0;
   var noScenario = true;
+  var $board = $("#container");
+
+  //this declares the starting player
+  var playerArray = ["x", "o"];
+  var randomIndex = Math.floor(Math.random() * 2);
+  var $player = playerArray[randomIndex];
 
   //this function builds the array of winning scenarios
+  //--> The math behind this function is explained in the README file
   function buildArray() {
     var startIndex = 0;
 
-    //this first loop adds possible horizontal wins to the array
+    //this first loop adds possible horizontal wins to the array 
     for (var i = 0; i < selectedSize; i++) {
       var arr2 = [];
       var currentIndex = startIndex;
@@ -28,7 +53,7 @@ $(document).ready(function () {
         currentIndex++;
       }
       arr3.push(arr2);
-      startIndex = startIndex + selectedSize;
+      startIndex += selectedSize;
     }
 
     //this loop adds possible vertical wins to the array
@@ -61,36 +86,32 @@ $(document).ready(function () {
     arr3.push(arr4);
   }
 
-  //this declares the starting player
-  var playerArray = ["x", "o"];
-  var randomIndex = Math.floor(Math.random() * 2);
-  var $player = playerArray[randomIndex];
-
-  //assign the board variable
-  var $board = $("#container");
-
-  // get grid size and user name 
+  //landing popup promotes user to choose a grid size and user name 
   var selectedSize;
   $("#choiceButton").click(function () {
-    selectedSize = $("input[name=gridNum]:checked").val();
-    selectedSize = parseInt(selectedSize);
-    var $playerOne = $("#playerOne").val();
-    sessionStorage.setItem("playerOne", $playerOne);
-
-    //close popup and create the grid
-    gridPopUp.style.display = "none";
-    createGrid();
+    selectedSize = parseInt($("input[name=gridNum]:checked").val());
+    sessionStorage.setItem("playerOne", $("#playerOne").val());
+    gridPopUp.style.display = "none"; // close popup
+    createGrid(); // create the grid
   });
 
 
   // this function alternates between player x and o
   function changePlayer() {
-    if ($player === "o") {
-      $player = "x";
-    } else if ($player === "x") {
-      $player = "o";
-      noScenario = true;
-      onePlayer();
+    $player === "o" ? $player = "x" : ($player = "o", noScenario = true, onePlayer());
+  }
+
+  // this function adds the token in the slot 
+  function addToken(slotPlayed) {
+    slotPlayed.removeClass("empty");
+    slotPlayed.css("cursor", "default");
+    slotPlayed.css("border", "none");
+    slotPlayed.css("background-color", "rgb(57, 148, 189)");
+    slotPlayed.addClass($player);
+    checkWin();
+    //if no win is achieved trigger next turn
+    if (!checkWin()) {
+      changePlayer();
     }
   }
 
@@ -104,6 +125,7 @@ $(document).ready(function () {
 
     // empty out the board before creating a new one
     $board.empty();
+
     //loop to create multiple rows
     for (var row = 0; row < selectedSize; row++) {
       var $row = $("<div>").addClass("row");
@@ -116,125 +138,63 @@ $(document).ready(function () {
       $board.append($row);
     }
 
-    //when player clicks a slot
-    $(".column.empty").on("click", function (event) {
-      if ($player === "x") {
-        //check if slot is empty
-        if ($(this).hasClass("empty")) {
-          $(this).removeClass("empty");
-          $(this).css("cursor", "default");
-          $(this).css("border", "none");
-          $(this).css("background-color", "rgb(57, 148, 189)");
-          $(this).addClass($player);
-          var winner = checkWin();
-          //if no win is achieved trigger next turn
-          if (!winner) {
-            changePlayer();
-          }
-        }
-        //if win is achieved display the winner
-        if (winner) {
-          // score counter
-          var scoreXCount = $("#numberOfXWins");
-          var scoreOCount = $("#numberOfOWins");
-          var xCurrentScore;
-          var oCurrentScore;
-          if ($player === "x") {
-            if (sessionStorage.getItem("xAIScore") === null) {
-              sessionStorage.setItem("xAIScore", 1);
-              xCurrentScore = sessionStorage.getItem("xAIScore");
-            } else {
-              xCurrentScore = parseInt(sessionStorage.getItem("xAIScore")) + 1;
-              sessionStorage.setItem("xAIScore", xCurrentScore);
-            }
-          } else if ($player === "o") {
-            if (sessionStorage.getItem("oAIScore") === null) {
-              sessionStorage.setItem("oAIScore", 1);
-              oCurrentScore = sessionStorage.getItem("oAIScore");
-            } else {
-              oCurrentScore = parseInt(sessionStorage.getItem("oAIScore")) + 1;
-              sessionStorage.setItem("oAIScore", oCurrentScore);
-            }
-          }
-          xCurrentScore = sessionStorage.getItem("xAIScore");
-          oCurrentScore = sessionStorage.getItem("oAIScore");
-          scoreXCount.text(xCurrentScore);
-          $("#numberOfXWins").append(scoreXCount);
-          scoreOCount.text(oCurrentScore);
-          $("#numberOfOWins").append(scoreOCount);
-          var playerName = sessionStorage.getItem("playerOne")
-
-          // popup winner
-
-          $(".popUp_Border>p").text("Congrats " + playerName + "!");
-          popUp.style.display = "block";
-        }
-      }
-    });
+    //if it's the A.I's turn go to the A.I function
     if ($player === 'o') {
       onePlayer();
-
     }
+
+    //when player clicks a slot check if it's empty and play
+    $(".column.empty").on("click", function (event) {
+      if ($player === "x") {
+        if ($(this).hasClass('empty')) {
+          addToken($(this));
+        }
+      }
+      if ($player === 'o') {
+        onePlayer();
+      }
+    });
   }
 
-  // this is a popup window that displays the result
-  var popUp = document.getElementById("winPopup");
-  var span = document.getElementsByClassName("close")[0];
-
+  // this function creates the A.I that plays against the player
   function onePlayer() {
-
+    var $slot = $(".column");
+    var current;
     var ruinPlayer = true;
 
-
-    if (moves === NaN || moves === undefined || moves === null) {
-      moves = selectedSize;
-    }
-
-    if (moves === selectedSize) {
-      if (selectedSize === 3 && $(".column").eq(4).hasClass("empty")) {
-        $(".column").eq(4).addClass("o");
-        $(".column").eq(4).css("border", "none");
-        $(".column").eq(4).css("background-color", "rgb(57, 148, 189)");
-        $(".column").eq(4).removeClass("empty");
-
+    // if it's the first move in the game
+    if (moves === 0) {
+      // if this is a 3x3 grid try to put it in the middle slot
+      if (selectedSize === 3 && $slot.eq(4).hasClass('empty')) {
+        addToken($slot.eq(4));
         current = 4;
         noScenario = false;
-        moves--;
-      } else {
-        for (var i = 0; i < $(".column").length; i++) {
-          random = Math.floor(Math.random() * ($(".column").length - 1));
-          if (
-            $(".column").eq(random).hasClass("empty")
-          ) {
-            $(".column").eq(random).addClass("o");
-            $(".column").eq(random).css("border", "none");
-            $(".column").eq(random).css("background-color", "rgb(57, 148, 189)");
-            $(".column").eq(random).removeClass("empty");
-
+        moves++;
+      } else { //if this isn't a 3x3 game or if the middle slot is not empty put the first token in a random empty slot
+        for (var i = 0; i < $slot.length; i++) {
+          random = Math.floor(Math.random() * ($slot.length - 1));
+          if ($slot.eq(random).hasClass('empty')) {
+            addToken($slot.eq(random));
             current = random;
             noScenario = false;
-            moves--;
+            moves++;
             break;
           }
         }
       }
-    } else if (selectedSize > moves) {
+    } else if (moves > 0) { //if this is not the first move
+      //check if possibility of immediate winning exists 
       loopWinner: for (var i = arr3.length - 1; i >= 0; i--) {
         var anotherCounter = 0;
-        loopWinner2: for (var j = 0; j < arr3[i].length; j++) {
-
-          if ($(".column").eq(arr3[i][j]).hasClass("o")) {
+        for (var j = 0; j < arr3[i].length; j++) {
+          if ($slot.eq(arr3[i][j]).hasClass("o")) {
             anotherCounter++
           }
           if (anotherCounter === (selectedSize - 1)) {
             for (var k = 0; k < arr3[i].length; k++) {
-              if ($(".column").eq(arr3[i][k]).hasClass("empty")) {
-                $(".column").eq(arr3[i][k]).removeClass("empty");
-                $(".column").eq(arr3[i][k]).addClass("o");
-                $(".column").eq(arr3[i][k]).css("border", "none");
-                $(".column").eq(arr3[i][k]).css("background-color", "rgb(57, 148, 189)");
+              if ($slot.eq(arr3[i][k]).hasClass("empty")) {
+                addToken($slot.eq(arr3[i][k]));
                 noScenario = false;
-                var winner = checkWinningArray2();
                 ruinPlayer = false;
                 break loopWinner;
               }
@@ -242,24 +202,18 @@ $(document).ready(function () {
           }
         }
       }
-
+      //check if possibility of other player's win is possible and stop it
       loopEvil: for (var i = arr3.length - 1; i >= 0; i--) {
         var anotherCounter = 0;
-        loopEvil2: for (var j = 0; j < arr3[i].length; j++) {
-          // debugger;
-
-          if ($(".column").eq(arr3[i][j]).hasClass("x")) {
+        for (var j = 0; j < arr3[i].length; j++) {
+          if ($slot.eq(arr3[i][j]).hasClass("x")) {
             anotherCounter++
           }
           if (anotherCounter === (selectedSize - 1)) {
             for (var k = 0; k < arr3[i].length; k++) {
-              if ($(".column").eq(arr3[i][k]).hasClass("empty")) {
-                $(".column").eq(arr3[i][k]).removeClass("empty");
-                $(".column").eq(arr3[i][k]).addClass("o");
-                $(".column").eq(arr3[i][k]).css("border", "none");
-                $(".column").eq(arr3[i][k]).css("background-color", "rgb(57, 148, 189)");
+              if ($slot.eq(arr3[i][k]).hasClass("empty")) {
+                addToken($slot.eq(arr3[i][k]));
                 noScenario = false;
-                var winner = checkWinningArray2();
                 ruinPlayer = false;
                 break loopEvil;
               }
@@ -267,26 +221,20 @@ $(document).ready(function () {
           }
         }
       }
-      // 
-
+      // if none of the above possibilities exist then check the placement of the last played toke "current" 
+      //and try to play in a slot that would form a winning scenario with that last token
       if (ruinPlayer) {
-        // 
         loop1: for (var i = arr3.length - 1; i >= 0; i--) {
-          loop2: for (var j = 0; j < selectedSize; j++) {
-            // debugger;
-            if (arr3[i][j] === random) {
+          for (var j = 0; j < selectedSize; j++) {
+            if (arr3[i][j] === current) {
               loop3: for (var k = 0; k < arr3[i].length; k++) {
                 for (var w = 0; w < arr3[i].length; w++) {
-                  if ($(".column").eq(arr3[i][w]).hasClass("x")) {
+                  if ($slot.eq(arr3[i][w]).hasClass("x")) {
                     break loop3;
-                  } else if ($(".column").eq(arr3[i][k]).hasClass("empty")) {
-                    $(".column").eq(arr3[i][k]).removeClass("empty");
-                    $(".column").eq(arr3[i][k]).addClass("o");
-                    $(".column").eq(arr3[i][k]).css("border", "none");
-                    $(".column").eq(arr3[i][k]).css("background-color", "rgb(57, 148, 189)");
+                  } else if ($slot.eq(arr3[i][k]).hasClass("empty")) {
+                    addToken($slot.eq(arr3[i][k]));
                     random = arr3[i][k];
                     noScenario = false;
-                    var winner = checkWinningArray2();
                     break loop1;
                   }
                 }
@@ -295,41 +243,30 @@ $(document).ready(function () {
           }
         }
       }
+      // if no winning scenarios are possible play in the first empty slot you find
       if (noScenario) {
-        for (var i = 0; i < $(".column").length; i++) {
-          if (
-            $(".column")
-              .eq(i)
-              .hasClass("empty")
-          ) {
-            $(".column")
-              .eq(i)
-              .addClass("o");
-            $(".column").eq(i).css("border", "none");
-            $(".column").eq(i).css("background-color", "rgb(57, 148, 189)");
-            $(".column")
-              .eq(i)
-              .removeClass("empty");
+        for (var i = 0; i < $slot.length; i++) {
+          if ($slot.eq(i).hasClass("empty")) {
+            addToken($slot.eq(i));
             noScenario = false;
-            var winner = checkWinningArray2();
-            moves--;
             break;
           }
         }
       }
     }
+  }
 
-    function checkWinningArray2() {
+  //this function checks whether a win is achieved
+  function checkWin() {
+    var $slot = $(".column");
+
+    // this will check the array for all possible wins
+    function checkWinningArray() {
       var counter = 0;
       for (var i = 0; i < arr3.length; i++) {
         for (var j = 0; j < arr3[i].length; j++) {
           var index = arr3[i][j];
-          if (
-            $(".column")
-              .eq(index)
-              .hasClass("o")
-          ) {
-            // console.log(arr3);
+          if ($slot.eq(index).hasClass($player)) {
             counter++;
           }
           if (counter === selectedSize) {
@@ -340,16 +277,14 @@ $(document).ready(function () {
       }
     }
 
-    var scoreXCount = $("#numberOfXWins");
-    var scoreOCount = $("#numberOfOWins");
-    var xCurrentScore;
-    var oCurrentScore;
-    if (winner) {
+    var winner = checkWinningArray();
 
-      if (sessionStorage.getItem("oAIScore") === null) {
-        sessionStorage.setItem("oAIScore", 1);
-        oCurrentScore = sessionStorage.getItem("oAIScore");
-      } else {
+    // if a win is achieved pop up the score and the winner
+    if (winner) {
+      if ($player === "x") {
+        xCurrentScore = parseInt(sessionStorage.getItem("xAIScore")) + 1;
+        sessionStorage.setItem("xAIScore", xCurrentScore);
+      } else if ($player === "o") {
         oCurrentScore = parseInt(sessionStorage.getItem("oAIScore")) + 1;
         sessionStorage.setItem("oAIScore", oCurrentScore);
       }
@@ -357,73 +292,34 @@ $(document).ready(function () {
       xCurrentScore = sessionStorage.getItem("xAIScore");
       oCurrentScore = sessionStorage.getItem("oAIScore");
       scoreXCount.text(xCurrentScore);
-      $("#numberOfXWins").append(scoreXCount);
       scoreOCount.text(oCurrentScore);
-      $("#numberOfOWins").append(scoreOCount);
+      var playerName = sessionStorage.getItem("playerOne")
 
-      $(".popUp_Border>p").text("Hahaha, I win!");
+      if ($player === "x") {
+        $(".popUp_Border>p").text("Congrats " + playerName + "!");
+      } else {
+        $(".popUp_Border>p").text("Hahaha, I win!");
+      }
       popUp.style.display = "block";
-    } else {
-      var newCounter = 0;
-      for (var i = 0; i < $(".column").length; i++) {
-        if (
-          $(".column")
-            .eq(i)
-            .hasClass("empty")
-        ) {
-          newCounter++;
-        }
-      }
-      if (newCounter === 0) {
-        xCurrentScore = sessionStorage.getItem("xAIScore");
-        oCurrentScore = sessionStorage.getItem("oAIScore");
-        scoreXCount.text(xCurrentScore);
-        $("#numberOfXWins").append(scoreXCount);
-        scoreOCount.text(oCurrentScore);
-        $("#numberOfOWins").append(scoreOCount);
-        $(".popUp_Border>p").text("Aw it's a draw");
-        popUp.style.display = "block";
-      }
-      changePlayer();
-    }
-  }
-
-  //this function checks whether a win is achieved
-  function checkWin() {
-    // this will check the array for all possible wins
-    function checkWinningArray() {
-      var counter = 0;
-      for (var i = 0; i < arr3.length; i++) {
-        for (var j = 0; j < arr3[i].length; j++) {
-          var index = arr3[i][j];
-          if (
-            $(".column").eq(index).hasClass("x")
-          ) {
-            counter++;
-          }
-          if (counter === selectedSize) {
-            return true;
-          }
-        }
-        counter = 0;
-      }
+      return true;
     }
 
     //this checks whether a draw is reached
     var newCounter = 0;
-    for (var i = 0; i < $(".column").length; i++) {
-      if (
-        $(".column").eq(i).hasClass("empty")
-      ) {
+    for (var i = 0; i < $slot.length; i++) {
+      if ($slot.eq(i).hasClass("empty")) {
         newCounter++;
       }
     }
     if (newCounter === 0) {
+      scoreXCount.text(xCurrentScore);
+      scoreOCount.text(oCurrentScore);
       $(".popUp_Border>p").text("Aw it's a draw");
       popUp.style.display = "block";
+      return true;
     }
 
-    return checkWinningArray();
+    return false;
   }
 
   //when popup x button is clicked close the popup
@@ -442,6 +338,4 @@ $(document).ready(function () {
   $(".replay").on("click", function () {
     location.reload();
   });
-
-
 });
